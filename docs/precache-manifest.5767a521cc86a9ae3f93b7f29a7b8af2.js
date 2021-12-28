8b35aad886d1372711d89bd392ec753dcffe33da
Albaro <alvaro>
2021-12-28 16:11:35 -0600

    first commit

---
+++ docs/precache-manifest.5767a521cc86a9ae3f93b7f29a7b8af2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de39a606fb315038516f3f527fd2c66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "532626e6e1fee39ce984",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "20209ad7da43a93a4980",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "532626e6e1fee39ce984",
+    "url": "/static/js/2.835d2913.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.835d2913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20209ad7da43a93a4980",
+    "url": "/static/js/main.3c11cbba.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
